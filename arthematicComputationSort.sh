02194d86ab92e6c6f5e89eb90f20236353e54760
alekhya87 <davulurialekhya87909.com>
2020-10-07 12:06:04 +0530

    Usecase6 is done

--- arthematicComputationSort.sh
+++ arthematicComputationSort.sh
@@ -20,3 +20,12 @@ echo $AritematicOperation3
 #usecase-5
 AritematicOperation4=$(($value1%$value2+$value3))
 echo $AritematicOperation4
+
+#usecase-6
+declare -A AritematicOperation
+AritematicOperation[1]=$AritematicOperation1
+AritematicOperation[2]=$AritematicOperation2
+AritematicOperation[3]=$AritematicOperation3
+AritematicOperation[4]=$AritematicOperation4
+
+echo "dictionary results are:" ${AritematicOperation[@]}
